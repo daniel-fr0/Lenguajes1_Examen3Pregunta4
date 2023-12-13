6a1c07bf5630bbf54882d60ab892f9ccdf8e4c56
daniel-fr0 <64498365+daniel-fr0@users.noreply.github.com>
2023-12-13 01:54:33 -0400

    tests listos para correr con script

--- tests/test_manejador.py
+++ tests/test_manejador.py
@@ -5,9 +5,23 @@ from src.manejador import main
 
 
 class TestManejador(unittest.TestCase):
-	def test_main(self):
+	def test_salir(self):
 		with patch('builtins.input', side_effect=['SALIR']):
 			with patch('sys.stdout', new_callable=io.StringIO) as fakeStdout:
 				main()
 				output = fakeStdout.getvalue().strip().split('\n')
 				self.assertEqual(output[1], 'Bienvenido al manejador de tablas de métodos virtuales. Este trabaja sobre un sistema')
+
+	def test_comando_desconocido(self):
+		with patch('builtins.input', side_effect=['ejecutar', 'salir']):
+			with patch('sys.stdout', new_callable=io.StringIO) as fakeStdout:
+				main()
+				output = fakeStdout.getvalue().strip().split('\n')
+				self.assertEqual(output[-1], 'Comando no reconocido.')
+
+	def test_comando_vacio(self):
+		with patch('builtins.input', side_effect=['', 'salir']):
+			with patch('sys.stdout', new_callable=io.StringIO) as fakeStdout:
+				main()
+				output = fakeStdout.getvalue().strip().split('\n')
+				self.assertEqual(output[-1], '-------------------------------------------------------------------------------------')
